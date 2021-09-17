2616c4f5472c28881f7a224496fcb7ed31c69d2f
Rony T <ronytarafder99@gmail.com>
2021-09-17 23:03:52 +0600

    Perfer for deploy

--- AwesomeBlog/settings.py
+++ AwesomeBlog/settings.py
@@ -134,3 +134,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+import django_heroku
+django_heroku.settings(locals())
